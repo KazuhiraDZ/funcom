14a7710310d33e15703fb97736abbeba33e769ed
Siyuan Jiang <sjiang1@nd.edu>
2018-05-14 14:24:58 -0400

    download bleu script instead of requiring MOSES package installed.

--- eval/bleu.sh
+++ eval/bleu.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+show_usage() {
+    echo -e "Usage: $0 reference_file prediction_file"
+}
+
+if [ $# -lt 2 ]
+then
+    show_usage
+    exit 1
+fi
+
 ref=$1
 pred=$2
 
-source ./configure.sh
-$mosesdecoder/scripts/generic/multi-bleu.perl $ref < $pred
+if [ ! -f 'multi-bleu.perl' ]; then
+    curl -O "https://raw.githubusercontent.com/moses-smt/mosesdecoder/RELEASE-4.0/scripts/generic/multi-bleu.perl"
+    echo "downloaded multi-bleu.perl from moses"
+fi
+
+perl ./multi-bleu.perl $ref < $pred
