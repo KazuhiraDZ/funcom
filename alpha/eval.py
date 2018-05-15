fdada8a0afad01fd3bbe32421bdb458e4f07debf
Siyuan Jiang <sjiang1@nd.edu>
2018-05-15 14:27:25 -0400

    Added beam search for prediction in eval.py

--- alpha/eval.py
+++ alpha/eval.py
@@ -33,6 +33,66 @@ def print_seq_str(seq, index_word):
         
     return print_str
 
+def beam_search(predict, source_sequence, coms_vocabsize, max_caplen, sos, eos, k):
+    dead_k = 0 # samples that reached eos
+    dead_samples = []
+    dead_scores = []
+
+    live_k = 1 # samples that did not yet reached eos
+    live_preds = [[]]
+    seq = np.zeros((max_caplen))
+    curs = [sos] # as a feed in the beginning (temporary workaround)
+    vhists = np.zeros((1, coms_vocabsize))
+    live_inputs = [np.asarray([source_sequence]),
+                   np.asarray(curs).reshape((-1, 1)),
+                   np.asarray([seq]),
+                   np.asarray(vhists)]
+    live_scores = [0]
+    word_cnt =  0
+
+    while live_k and dead_k < k:
+        probslist = predict(live_inputs)
+        # for each row in live_score, compute #coms_vocabsize scores for each word in probslist
+        cand_scores = np.array(live_scores)[:,None] - np.log(probslist)
+        
+        # pick the best scores
+        cand_flat = cand_scores.flatten()
+        ranks_flat = np.argpartition(cand_flat, k)[:k]
+        live_scores = cand_flat[ranks_flat]
+        live_preds = [live_preds[r//coms_vocabsize]+[r%coms_vocabsize] for r in ranks_flat]
+        
+        # update live_preds, dead_samples
+        zombie = [s[-1] == eos or len(s) >= max_caplen for s in live_preds]
+        dead_samples += [s for s,z in zip(live_preds,zombie) if z]
+        dead_scores += [s for s,z in zip(live_scores,zombie) if z]
+        dead_k = len(dead_samples)
+        live_preds = [s for s,z in zip(live_preds,zombie) if not z]
+        live_scores = [s for s,z in zip(live_scores,zombie) if not z]
+        live_k = len(live_preds)
+
+        # update live_inputs
+        seq = np.zeros((max_caplen))
+        seq[word_cnt] = 1
+        new_vhists = np.zeros((len(ranks_flat), coms_vocabsize))
+        for r in ranks_flat:
+            new_vhists[r//coms_vocabsize][r%coms_vocabsize] = 1
+            
+        vhists = [np.logical_or(vhists[i//coms_vocabsize], j) for i,j in zip(ranks_flat,new_vhists)]
+        vhists = [s for s,z in zip(vhists,zombie) if not z]
+        curs = [r%coms_vocabsize for r in ranks_flat]
+        curs = [s for s,z in zip(curs,zombie) if not z]
+        
+        live_inputs = [np.asarray([source_sequence for i in range(live_k)]),
+                       np.asarray(curs).reshape((-1, 1)),
+                       np.asarray([seq for i in range(live_k)]),
+                       np.asarray(vhists)]
+
+        word_cnt += 1
+        # end prediction
+
+    return dead_samples[np.argmax(dead_scores)]
+
+    
 ## greedy search
 def greedy_search(predict, source_sequence, coms_vocabsize, max_caplen, sos, eos):
     search_logger = logging.getLogger(__name__ + ': greedy_search')
@@ -103,24 +163,42 @@ def parse_config(configfile):
 def parse_args():
     parser = argparse.ArgumentParser(description='Run models to predict comments.')
     parser.add_argument('--config', nargs=1, help='the config file, see test.ini as an example', required=True)
+    parser.add_argument('--beamsearch', metavar='K', type=int, nargs=1, help='use beam search, and set K as beam width; if not used, run greedy search')
     parser.add_argument('model', nargs='+', help='a model\'s file path')
     args = parser.parse_args()
     modelpath = args.model[0]
     configfile = args.config[0]
+    beamsearch = -1
+        
     logger.info("input model: " + modelpath)
     logger.info("config file: " + configfile)
+    
+    if args.beamsearch:
+        beamsearch = args.beamsearch[0]
+        logger.info("beamsearch, k=" + str(beamsearch))
+    else:
+        logger.info("greedysearch")
+
     return {'modelpath':modelpath,
-            'configfile':configfile,}
+            'configfile':configfile,
+            'beamsearch':beamsearch,}
 
     
 if __name__ == '__main__':
     args = parse_args()
-    modelpath = args['modelpath']
-    config = parse_config(args['configfile'])
-    dataprep = config['dataprep']
-    embfile  = config['embfile']
-    outdir   = config['outdir']
-    timestr = time.strftime("%Y%m%d-%H%M%S")
+    modelpath  = args['modelpath']
+    beamwidth = args['beamsearch']
+    search = ''
+    if beamwidth == -1:
+        search = 'greedy'
+    else:
+        search = 'beam'
+    
+    config     = parse_config(args['configfile'])
+    dataprep   = config['dataprep']
+    embfile    = config['embfile']
+    outdir     = config['outdir']
+    timestr    = time.strftime("%Y%m%d-%H%M%S")
     outputfile = {'srcfile':os.path.join(outdir, 'testsrc') + timestr + '.txt',
                   'reffile':os.path.join(outdir, 'testref') + timestr + '.txt',
                   'predict':os.path.join(outdir, 'predicts') + timestr + '.txt',}
@@ -205,15 +283,24 @@ if __name__ == '__main__':
 
     logger.info('model predicting ...')
     predlist = []
+    
     for i in range(len(datslist)):
         if sos == -1:
             sos = comslist[i][0]
+
+        if search == 'greedy':
+            prediction = greedy_search(model.predict, datslist[i], coms_vocabsize, max_comlen, sos, eos)
+        elif search == 'beam':
+            logger.info('beam search...')
+            prediction = beam_search(model.predict, datslist[i], coms_vocabsize, max_comlen, sos, eos, beamwidth)
+        else:
+            logger.error("cannot recognize search algorithm: " + search + ", exit.")
+            sys.exit()
         
-        prediction = greedy_search(model.predict, datslist[i], coms_vocabsize, max_comlen, sos, eos)
         prediction_str = print_seq_str(prediction, index_word_com) + "\n"
         logger.info(str(i+1) + '/' + str(len(datslist)) + ' prediction: ' + prediction_str)
         predlist.append(prediction)
-        
+
     logger.info('finished prediction. generated ' + str(len(predlist)) + ' sequences')
     prediction_str = "\n".join([print_seq_str(prediction, index_word_com) for prediction in predlist])
     print(prediction_str, file=open(outputfile['predict'], "w"))
