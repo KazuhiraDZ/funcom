fab7777fc2e427f47eaf9667bd2752db97d3fda8
Siyuan Jiang <sjiang1@nd.edu>
2018-05-22 21:01:16 -0400

    make test nematus work.

--- baseline-nematus/test.sh
+++ baseline-nematus/test.sh
@@ -1,3 +1,97 @@
 #!/bin/bash
 
-time bash test.sh workdir/models/model.npz-30000 workdir/test.src.txt workdir/predict.txt workdir/models/model.npz.json
+source time.sh
+
+today=`date +%Y-%m-%d.%H%M%S`
+log=test.log.$today
+
+read -r -d '' helpmsg <<EOM
+Usage: $0
+    -c          [required] set a config file
+    -d          [optional] set a gpu id to be used
+    -l          [optional] set the file name for log; default file name: $log
+    -h          display this help message
+EOM
+
+passarg=true
+while getopts ":c:d:l:h" opt; do
+  case ${opt} in
+    c )
+	config=$OPTARG
+	;;
+    d )
+	dev=$OPTARG
+	;;
+    l )
+	log=$OPTARG
+	;;
+    h )
+        passarg=false
+	;;
+    \? )
+	echo "Invalid option: $OPTARG" 1>&2
+	passarg=false
+	;;
+    : )
+	echo "Invalid option: $OPTARG requires an argument" 1>&2
+	passarg=false
+	;;
+  esac
+done
+shift $((OPTIND -1))
+
+if $passarg && [ -z "$config" ]; then
+    echo "Must use -c to specify a config file to use."
+    passarg=false
+fi
+
+if ! $passarg ;then
+    echo "$helpmsg"
+    exit 0
+fi
+
+echo "config file: $config, log file: $log" | tee -a $log
+
+source download_nematus.sh
+
+###
+### test nematus
+###
+function checkconfig()
+{
+    local var=$1
+    if [ -z "${TEST[$var]}" ]; then
+        echo "$0: cannot get config variable: $var for test_nmt.sh. Exit."
+        exit 1
+    fi
+    printf "$var: ${TEST[$var]}, " | tee -a $log
+}
+
+eval "$(cat $config  | python ./ini2arr.py)"
+
+printf "test_nmt.sh: " | tee -a $log
+checkconfig 'outdir'
+mkdir -p ${TEST[outdir]}
+checkconfig 'modeldir'
+checkconfig 'datadir'
+checkconfig 'predict'
+printf "\n" | tee -a $log
+modelfiles=`python3 getmodels.py ${TEST[modeldir]}`
+if [ -z "$modelfiles" ]; then
+    echo "Cannot find model files in ${TEST[modeldir]}. Exit." | tee -a $log
+    exit 1
+else
+    echo "model files: $modelfiles" | tee -a $log    
+fi
+
+start=$(date +%s.%N)
+if [ -z "$dev" ]; then
+    bash test_nmt.sh "$modelfiles" ${TEST[datadir]}/test.src.txt ${TEST[outdir]}/${TEST[predict]} ${TEST[modeldir]}/model.npz.json 2>&1 | tee -a $log
+else
+    CUDA_VISIBLE_DEVICES=$dev bash test_nmt.sh "$modelfiles" ${TEST[datadir]}/test.src.txt ${TEST[outdir]}/${TEST[predict]} ${TEST[modeldir]}/model.npz.json 2>&1 | tee -a $log
+fi
+end=$(date +%s.%N)
+diff=`show_time $end $start`
+echo "test: $diff" | tee -a $log
+
+exit 0
--- baseline-nematus/train.sh
+++ baseline-nematus/train.sh
@@ -2,7 +2,7 @@
 source time.sh
 
 today=`date +%Y-%m-%d.%H%M%S`
-log=run.log.$today
+log=train.log.$today
 
 read -r -d '' helpmsg <<EOM
 Usage: $0
@@ -46,43 +46,12 @@ fi
 
 if ! $passarg ;then
     echo "$helpmsg"
+    exit 0
 fi
 
 echo "config file: $config, log file: $log" | tee -a $log
 
-
-###
-### download nematus if necessary
-###
-function downloadnematus()
-{
-    echo "git submodule does not work... it's okay..."
-
-    local nematusdir=nematus-tensorflow
-    if [ -d "$nematusdir" ]; then
-	echo "nematus-tensorflow already exists. if you would like to update the nematus copy, remove the folder first." | tee -a $log
-    else
-	echo "we download from my fork..." | tee -a $log
-	# use my fork of nematus, in case there is any major change in the original nematus repo
-	git clone --depth=1 --branch=tensorflow https://github.com/sjiang1/nematus.git $nematusdir
-	rm -rf ${nematusdir}/.git
-    fi
-}
-
-git submodule init
-submoduleflag=false
-if [ $? -eq 0 ]
-then
-    git submodule update
-    if [ $? -eq 0 ]
-    then
-	submoduleflag=true
-    fi
-fi
-
-if ! $submoduleflag ;then
-    downloadnematus
-fi
+source download_nematus.sh
 
 ###
 ### prepare the data set
