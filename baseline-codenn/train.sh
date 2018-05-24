b56750d504d218bbf428758dd2d2418a493c4c9e
Siyuan Jiang <2203934+sjiang1@users.noreply.github.com>
2018-05-24 15:42:48 +0000

    updated train.sh to take gpu id as an input run main.lua directly from train.sh instead of run.sh

--- baseline-codenn/train.sh
+++ baseline-codenn/train.sh
@@ -10,15 +10,19 @@ log=train.log.$today
 read -r -d '' helpmsg <<EOM
 Usage: $0
     -c          [required] set a config file
+    -d          [required] set a gpu id to be used
     -l          [optional] set the file name for log; default file name: $log
     -h          display this help message
 EOM
 
 passarg=true
-while getopts ":c:l:h" opt; do
+while getopts ":c:d:l:h" opt; do
   case ${opt} in
     c )
 	config=$OPTARG
+	;;
+    d )
+	dev=$OPTARG
 	;;
     l )
 	log=$OPTARG
@@ -40,6 +44,11 @@ shift $((OPTIND -1))
 
 if $passarg && [ -z "$config" ]; then
     echo "Must use -c to specify a config file to use."
+    passarg=false
+fi
+
+if $passarg && [ -z "$dev" ]; then
+    echo "Must use -d to specify a gpu to use."
     passarg=false
 fi
 
@@ -153,9 +162,10 @@ else
     start=$(date +%s.%N)
     echo "running codenn/src/model/run.sh ... " | tee -a $log
     pushd ./codenn/src/model
-    bash ./run.sh cpp $cwd/$modelout  2>&1 | tee -a $cwd/$log
+    th ./main.lua -gpuidx $dev -language cpp -outdir $modelout
     popd
     end=$(date +%s.%N)
     diff=`show_time $end $start`
     echo "codenn/src/model/run.sh done: $diff" | tee -a $log
 fi
+
