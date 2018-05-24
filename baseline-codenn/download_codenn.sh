085b7de29a491bb3f39740693dc3f4f14dbf95c6
Siyuan Jiang <2203934+sjiang1@users.noreply.github.com>
2018-05-24 18:01:03 +0000

    fixed a small bug in download codenn

--- baseline-codenn/download_codenn.sh
+++ baseline-codenn/download_codenn.sh
@@ -9,7 +9,7 @@ function downloadcodenn()
     echo "git submodule does not work... it's okay..." | tee -a $log
 
     local codenndir=codenn
-    if [ -d "$codenndir" ]; then
+    if [ -d "$codenndir" ] && [ -f "$codenndir/src/cpp/CppTemplate.py" ]; then
 	echo "$codenndir already exists. if you would like to update the codenn copy, remove the folder first." | tee -a $log
     else
 	echo "we download from my fork..." | tee -a $log
