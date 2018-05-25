45651279d0463e34d00e9c60dc5fdef2d7be9485
Siyuan Jiang <sjiang1@nd.edu>
2018-05-25 12:08:35 -0400

    adding arguments to predict.lua in test.sh

--- baseline-codenn/test.sh
+++ baseline-codenn/test.sh
@@ -103,7 +103,7 @@ else
     start=$(date +%s.%N)
     echo "running codenn/src/model/predict.lua ... " | tee -a $log
     pushd ./codenn/src/model
-    th predict.lua -encoder ${TEST[modeldir]}/cpp.encoder -decoder ${TEST[modeldir]}/cpp.decoder -beamsize ${TEST[beamsize]} -gpuidx $dev -language cpp
+    th predict.lua -encoder ${TEST[modeldir]}/cpp.encoder -decoder ${TEST[modeldir]}/cpp.decoder -beamsize ${TEST[beamsize]} -gpuidx $dev -language cpp -outdir $predictout -outfile $predictfile
     popd
     end=$(date +%s.%N)
     diff=`show_time $end $start`
