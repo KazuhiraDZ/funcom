5992787cedadb6eabedc7d024c330616653ebb6b
Siyuan Jiang <sjiang1@nd.edu>
2018-06-18 14:50:52 -0400

    added an option of the number of models to be used for testing updated test.sh for spliting test files as necessary

--- baseline-nematus/test.sh
+++ baseline-nematus/test.sh
@@ -89,8 +89,9 @@ mkdir -p ${TEST[outdir]}
 checkconfig 'modeldir'
 checkconfig 'datadir'
 checkconfig 'predict'
+checkconfig 'modelnum'
 infoecho "\n"
-modelfiles=`python3 getmodels.py ${TEST[modeldir]}`
+modelfiles=`python3 getmodels.py ${TEST[modeldir]} ${TEST[modelnum]}`
 if [ -z "$modelfiles" ]; then
     error "Cannot find model files in ${TEST[modeldir]}. Exit."
     exit 1
@@ -101,7 +102,7 @@ fi
 start=$(date +%s.%N)
 function runtest(){
     local testfile=$1
-    infoecho "runtest: $testfile\n"
+    infoecho "runtest: $testfile, GPU device: $dev\n"
     CUDA_VISIBLE_DEVICES=$dev bash test_nmt.sh "$modelfiles" $testfile ${testfile}.predict ${TEST[modeldir]}/model.npz.json 2>&1 | tee -a $log    
 }
 
@@ -109,48 +110,83 @@ mkdir -p ${TEST[datadir]}/testsplitfiles
 if [ "$(ls -A ${TEST[datadir]}/testsplitfiles)" ]; then
     warning "the testsplitfiles are not empty. Will use the existing test files AND prediction files!"
 else
-    split -a 4 -d -l 3000 ${TEST[datadir]}/test.src.txt ${TEST[datadir]}/testsplitfiles/test.src.txt_
+    split -a 5 -d -l 100 ${TEST[datadir]}/test.src.txt ${TEST[datadir]}/testsplitfiles/test.src.txt_
 fi
 
 if [ -f ${TEST[outdir]}/${TEST[predict]} ]; then
     mv ${TEST[outdir]}/${TEST[predict]} "${TEST[outdir]}/${TEST[predict]}".old.$today
 fi
-rm -f "${TEST[outdir]}/${TEST[predict]}".duplicate
+rm -f "${TEST[outdir]}/${TEST[predict]}".src
 
-ls -v `find ${TEST[datadir]}/testsplitfiles/ -name "test.src.txt_[0-9][0-9][0-9][0-9]"` | while read line; do
+ls -v `find ${TEST[datadir]}/testsplitfiles/ -name "test.src.txt_[0-9][0-9][0-9][0-9][0-9]"` | while read line; do
     filename=$line
-    infoecho "running nematus on $filename ...\n"
+    warning "running nematus on $filename ...\n"
     if [ -s ${filename}.predict ]; then
     	warning "Skip running nematus. Using the existing ${filename}.predict"
-    	cat ${filename}.predict >> ${TEST[outdir]}/${TEST[predict]}
-    	cat $filename >> "${TEST[outdir]}/${TEST[predict]}".duplicate
+    	cat ${filename}.predict >> "${TEST[outdir]}/${TEST[predict]}"
+    	cat $filename >> "${TEST[outdir]}/${TEST[predict]}".src
     	continue
     fi
+
+    if [ ! -s ${filename}.predict.tmp ]; then
+	runtest "$filename";
+    fi
     
-    runtest "$filename";
-    if [ -s ${filename}.predict ]
-    then
-    	# cat ${filename}.predict >> ${TEST[outdir]}/${TEST[predict]}
-	rm -fr tmp
-	sed 's!<s>\s!!g' ${filename}.predict > tmp
-	sed 's!\s</s>\s*!!g' tmp >>  ${TEST[outdir]}/${TEST[predict]}
+    if [ -s ${filename}.predict ]; then
+	# rm -fr tmp # sed 's!<s>\s!!g' ${filename}.predict > tmp # sed 's!\s</s>\s*!!g' tmp >> "${TEST[outdir]}/${TEST[predict]}"
+	cat ${filename}.predict >> "${TEST[outdir]}/${TEST[predict]}"
     	cat $filename >> "${TEST[outdir]}/${TEST[predict]}".src
     else
-    	warning "Nematus did not generate prediciton file for $filename. Try splitting it again."
-	rm -f ${filename}_*
-    	split -a 4 -d -l 50 $filename ${filename}_
-    	ls -v ${filename}_* | while read smaller_filename; do
-    	    runtest "$smaller_filename"
-    	    if [ -s ${smaller_filename}.predict ]
-    	    then
-    		# cat ${smaller_filename}.predict >> ${TEST[outdir]}/${TEST[predict]}
-		rm -fr tmp
-		sed 's!<s>\s!!g' ${smaller_filename}.predict > tmp
-		sed 's!\s</s>\s*!!g' tmp >>  ${TEST[outdir]}/${TEST[predict]}
+    	rm -fr ${filename}.predict.tmp
+
+	warning "Nematus did not generate prediciton file for $filename. Try splitting it again."
+    	split -a 5 -d -l 5 $filename ${filename}_
+	tmpfilename="`basename $filename`"	
+	ls -v `find ${TEST[datadir]}/testsplitfiles/ -name "${tmpfilename}_[0-9][0-9][0-9][0-9][0-9]"` | while read smaller_filename; do
+	    warning "running nematus on ${smaller_filename} ...\n"
+	    
+	    if [ -s ${smaller_filename}.predict ]; then
+		warning "Skip running nematus. Using the existing ${smaller_filename}.predict"
+    		cat ${smaller_filename}.predict >> "${TEST[outdir]}/${TEST[predict]}"
+		cat ${smaller_filename} >> "${TEST[outdir]}/${TEST[predict]}".src
+		cat ${smaller_filename}.predict >> ${filename}.predict.tmp
+    		continue
+	    fi
+
+	    if [ ! -s ${smaller_filename}.predict.tmp ]; then
+		runtest "$smaller_filename";
+	    fi
+	    
+    	    if [ -s ${smaller_filename}.predict ]; then
+		cat ${smaller_filename}.predict >> "${TEST[outdir]}/${TEST[predict]}"
     		cat ${smaller_filename} >> "${TEST[outdir]}/${TEST[predict]}".src
+		cat ${smaller_filename}.predict >> ${filename}.predict.tmp
     	    else
-    		error "Nematus still cannot generate predictions for $smaller_filename "
-    		exit 1
+		warning "Nematus did not generate prediciton file for ${smaller_filename}. The final try: splitting it into one-line file."
+		split -a 5 -d -l 1 ${smaller_filename} ${smaller_filename}_*
+		tmpsmaller_filename="`basename ${smaller_filename}`"
+		ls -v `find ${TEST[datadir]}/testsplitfiles/ -name "${tmpsmaller_filename}_[0-9][0-9][0-9][0-9][0-9]"` | while read smallest_filename; do
+		    warning "running nematus on ${smallest_filename} ... \n"
+
+		    if [ -s ${smallest_filename}.predict ]; then
+			warning "Skipping running nematus. Using the existing ${smallest_filename}.predict"
+			cat ${smallest_filename}.predict >> "${TEST[outdir]}/${TEST[predict]}"
+			cat ${smallest_filename} >> "${TEST[outdir]}/${TEST[predict]}".src
+			cat ${smallest_filename}.predict >> ${smaller_filename}.predict.tmp
+			continue
+		    fi
+
+		    runtest ${smallest_filename}
+
+		    if [ -s ${smallest_filename}.predict ]; then
+			cat ${smallest_filename}.predict >> "${TEST[outdir]}/${TEST[predict]}"
+			cat ${smallest_filename} >> "${TEST[outdir]}/${TEST[predict]}".src
+			cat ${smallest_filename}.predict >> ${smaller_filename}.predict.tmp
+		    else
+    			error "Nematus still cannot generate predictions for $smallest_filename."
+		    fi
+		done
+    		
     	    fi
     	done
 	[[ $? != 0 ]] && exit $?
