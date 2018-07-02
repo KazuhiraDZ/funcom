ca25ef5bc928188806f7375de928b63a0bee1e98
Siyuan Jiang <sjiang1@nd.edu>
2018-07-02 15:10:45 -0400

    refactored training scripts for baseline-codenn

--- baseline-codenn/util.sh
+++ baseline-codenn/util.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+function absolutepath(){
+    local inpath=$1
+    local cwd=$2
+    if [[ "$inpath" = /* ]]; then
+        echo "$inpath"
+    else
+        echo "$cwd/$inpath"
+    fi
+}
+
 # copied from: https://stackoverflow.com/questions/12199631/convert-seconds-to-hours-minutes-seconds
 function show_time () {
     diff=$(echo "$1 - $2" | bc)
@@ -27,3 +37,11 @@ function show_time () {
     fi
     echo "${day}d ${hour}h ${min}m ${sec}s"
 }
+
+RED='\033[0;31m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+function infoecho(){ printf "$1" | tee -a $log; }
+function warning(){ echo -e "${YELLOW}Warning: $1${NC}" | tee -a $log; }
+function error(){ echo -e "${RED}Error: $1${NC}" | tee -a $log; }
