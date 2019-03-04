190425de3273cce30755bfea8d59f01a90ad0594
Collin McMillan <cmc@nd.edu>
2019-03-04 12:06:09 -0500

    model cleanup

--- model.py
+++ model.py
@@ -7,15 +7,7 @@ import tensorflow as tf
 from models.attendgru import AttentionGRUModel
 from models.ast_attendgru_xtra import AstAttentionGRUModel as xtra
 from models.transformer import TransformerModel
-from models.cmc3 import Cmc3Model as cmc3
-from models.cmc4 import Cmc4Model as cmc4
 from models.cmc5 import Cmc5Model as cmc5
-from models.cmc7 import Cmc7Model as cmc7
-from models.cmc8 import Cmc8Model as cmc8
-from models.cmc9 import Cmc9Model as cmc9
-from models.cmc10 import Cmc10Model as cmc10
-from models.cmc11 import Cmc11Model as cmc11
-from models.cmc12 import Cmc12Model as cmc12
 
 def create_model(modeltype, config):
     mdl = None
